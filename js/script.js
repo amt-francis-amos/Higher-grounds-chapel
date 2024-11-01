ac60f12dd8d3110bd11d40c068f2e0cc544bdc06
amt-francis-amos <sugartone45@yahoo.com>
2024-11-01 06:06:17 -0800

    commit to main

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+setInterval(() => {
+  slideIndex++;
+  showSlides(slideIndex);
+}, 5000); 
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.querySelectorAll(".slide");
+  const dots = document.querySelectorAll(".dot");
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  slides.forEach((slide, i) => {
+    slide.style.display = i + 1 === slideIndex ? "block" : "none";
+    dots[i].classList.toggle("active", i + 1 === slideIndex);
+
+ 
+    const bg = slide.getAttribute('data-background');
+    if (bg) {
+      slide.style.backgroundImage = `url(${bg})`;
+    }
+  });
+}
+
+
+
+
+
+
+// ---- MENU -----
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  const menuIcon = document.querySelector('.menu-icon i');
+
+  navLinks.classList.toggle('show');
+
+
+  if (navLinks.classList.contains('show')) {
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-times');
+  } else {
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
+  }
+}
